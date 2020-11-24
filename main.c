8361ba1b51335fb94b1284cfb0844cc70849f9ba
juhee67 <55428174+juhee67@users.noreply.github.com>
2020-11-24 22:55:13 +0900

    othello_1

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 6
+
+int gameboard[N][N];
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main (){
+	
+	char gameboard[N][N] = {0};
+	int row = 0;
+	int col = 0;
+	
+}
+
+
+
